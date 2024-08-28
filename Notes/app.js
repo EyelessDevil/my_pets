23c9298068e3bdafc96a4913d87d6acfb72639cf
LiRo <79753756+EyelessDevil@users.noreply.github.com>
2024-08-29 00:18:07 +0300

    init

---
+++ Notes/app.js
@@ -0,0 +1,67 @@
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+
+const notes = [
+    {
+        title: 'Выучить за лето JavaScript',
+        completed: true,
+    }, 
+    {
+        title: 'Выучить HTML & CSS',
+        completed: true,
+    },
+    {   title:'Устроится на нормальную работу, а не в такси херачить',
+        completed: false,
+    }
+] 
+
+function render() {
+    listElement.innerHTML = ''
+    if(notes.length === 0) {
+        listElement.innerHTML = '<p>Поздравляю! У тебя нет целей в жизни</p>'
+    }
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+    }
+}  
+render()
+
+listElement.onclick = function(event) {
+    if(event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+        if (type=== 'toggle') {
+            notes[index].completed = !notes[index].completed
+        } else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+    }
+    render()
+}
+
+function getNoteTemplate(note, index) {
+    return `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+          <span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger"data-index="${index}" data-type="remove">&times;</span>
+          </span>
+        </li>
+    `
+}
+
+createBtn.onclick = function(){
+    if(inputElement.value.length === 0) {
+        return  
+    }
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+
+    notes.push(newNote)
+    render()
+    inputElement.value = ''
+}
